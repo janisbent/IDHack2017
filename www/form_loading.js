b320bebe787ecee32cc96a12948cbd516d182dea
Alex Nolte <alexander.nolte@tufts.edu>
2017-04-08 04:19:15 -0400

    added group addition

--- www/form_loading.js
+++ www/form_loading.js
@@ -25,7 +25,42 @@ var person_form = '<hr> <form id="person_form" method="post" action="javascript:
         '</p> ' +
     '</form> <hr>';
 
+var group_form = '<form id="group_form" method="post" action="javascript:display_group()">' +
+        '<p>' +
+            '<hr>'+
+            'Name: <input type="text" name="gname" maxlength="60"/> '+
+            '<br> About this group: <br> <textarea name="gbio" rows="4" cols="60"> </textarea>'+
+            '<br>'+
+            '<button>Add Group</button>'+
+            '<br>'+
+            '<button type="submit" value="OK" onclick="javascript:close_form()">Done Adding Groups </button>'+
+            '<div id="groups"></div>'+
+            '<hr>'+
+        '</p>'+
+    '</form>';
+
 var graph_data = [];
+var groups = [];
+var people = [];
+
+function display_group(){
+    add_group();
+    var groupstring = "Groups:";
+
+    groups.map(function(group){
+        groupstring = groupstring + "<br>" + group.name;
+    });
+
+    $("#form").html(group_form);
+
+    $("#groupdata").html(groupstring);
+}
+
+function add_group(){
+    name = $("input[name='gname']").val();
+    bio = $("input[name='gbio'").val();
+    groups.push({"name": name, "bio": bio});
+}
 
 function display_data() {
     var gender = $("input[name='gender']:checked").val();
@@ -34,21 +69,39 @@ function display_data() {
     var bio    = $("input[name='bio']").val();
     var inf    = $("input[name='influence']").val();
 
+    people.push({"name": name, "age": age, "gender":gender,
+                 "influence": inf, "bio": bio});
+
     var informative = name + ", " + gender + ", " + age + " influence: " + inf;  
     var new_dataset = $('#dataset').html() + "<br>" + informative;
     $('#dataset').html(new_dataset);
 
-    $('#form').hide();
-    $('.option').show();
+    close_form();
 
     $('#form').html(person_form);
+}
+
+
+function close_form(form){
+    $('#form').hide();
+    $('.option').show();
+}
+
+function open_form(form){
+    $('#form').html(form);
+    $('#form').show();
+    $('.option').hide();
 }
 
 $(document).ready(function(){
     $('#form').html(person_form);
     $('#form').hide();
+
     $("#person_enter").click(function(){
-        $('#form').show();
-        $('.option').hide();
+        open_form(person_form);
     });
+
+    $("#village_setup").click(function(){
+        open_form(group_form);
+    })
 });
