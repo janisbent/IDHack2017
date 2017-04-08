19c55a883e93b66958ffa08f37059501b8c8c98a
Isaiah Mindich <immindich@gmail.com>
2017-04-08 04:40:48 -0400

    make id name in graph exporter

--- mysite/graphs/graph_exporter.py
+++ mysite/graphs/graph_exporter.py
@@ -12,9 +12,9 @@ from django.core import serializers
 import graphs
 
 def export_person(p):
-    return {'name' : p.name, 'bio' : p.desc,
+    return {'id' : p.name, 'bio' : p.desc,
             'gender' : p.gender, 'age' : p.age,
-            'id' : p.id}
+            'db_id' : p.id}
 
 def export_person_links(v, index_mapping):
     links = PersonRelationship.objects.filter(p1__village__id=v.id)
@@ -26,7 +26,7 @@ def export_village(v):
     nodes = [export_person(p) for p in v.person_set.all()]
     index_mapping = {}
     for i in range(len(nodes)):
-        index_mapping[nodes[i]['id']] = i
+        index_mapping[nodes[i]['db_id']] = i
     node_obj =  {'name' : v.name, 'desc' : v.desc,
                  'person_graph': {'nodes' : nodes,
                                   'links' : export_person_links(v, index_mapping)}}
